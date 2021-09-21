aa1ff082989725b6b6c338b44463050261aed9f3
sth32 <sth32@qq.com>
2021-09-21 16:49:53 +0800

    两个VAO，两个三角形

--- learnopengl/learngl.cpp
+++ learnopengl/learngl.cpp
@@ -23,6 +23,7 @@ int rectangle_EBO(GLFWwindow* window, unsigned int shaderProgram);
 
 // 练习题
 int two_triangle(GLFWwindow* window, unsigned int shaderProgram);
+int two_VAO_triangle(GLFWwindow* window, unsigned int shaderProgram);
 
 int main()
 {
@@ -87,7 +88,8 @@ int main()
 
     //return triangle_VAO(window, shaderProgram);
     //return rectangle_EBO(window, shaderProgram);
-    return two_triangle(window, shaderProgram);
+    //return two_triangle(window, shaderProgram);
+    return two_VAO_triangle(window, shaderProgram);
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -142,9 +144,9 @@ int rectangle_EBO(GLFWwindow* window, unsigned int shaderProgram)
 {
     float vertices[] = {
     0.5f, 0.5f, 0.0f,   // 右上角
-    0.5f, -0.5f, 0.0f,  // 右下角
-    -0.5f, -0.5f, 0.0f, // 左下角
-    -0.5f, 0.5f, 0.0f   // 左上角
+    0.5f,-0.5f, 0.0f,  // 右下角
+   -0.5f,-0.5f, 0.0f, // 左下角
+   -0.5f, 0.5f, 0.0f   // 左上角
     };
     unsigned int indices[] = { // 注意索引从0开始! 
         0, 1, 3, // 第一个三角形
@@ -190,10 +192,10 @@ int two_triangle(GLFWwindow* window, unsigned int shaderProgram)
     // 两个相连的三角
     float vertices[] = {
         -0.5f, -0.5f, 0.0f,
-         -0.25f,  0.5f, 0.0f,
+        -0.25f, 0.5f, 0.0f,
          0.0f, -0.5f, 0.0f,
          0.25f, 0.5f, 0.0f, // 增加两个点
-         0.5f, -0.5f,0.0f
+         0.5f, -0.5f, 0.0f
     };
     unsigned int VBO;
     glGenBuffers(1, &VBO);
@@ -217,6 +219,66 @@ int two_triangle(GLFWwindow* window, unsigned int shaderProgram)
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glDrawArrays(GL_TRIANGLES, 2, 3);   // 第二个三角的起始索引为2，还是画三个点
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+
+}
+
+int two_VAO_triangle(GLFWwindow* window, unsigned int shaderProgram)
+{
+    // 使用两个VAO画两个三角
+    // 想将获取VAO封装为函数，但失败了，直接复制了两份代码
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        -0.25f, 0.5f, 0.0f,
+         0.0f, -0.5f, 0.0f,
+    };
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    float vertices_1[] = {
+         0.5f,  0.5f, 0.0f,
+         0.25f,-0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f,
+    };
+
+    unsigned int VBO_1;
+    glGenBuffers(1, &VBO_1);
+    unsigned int VAO_1;
+    glGenVertexArrays(1, &VAO_1);
+
+    glBindVertexArray(VAO_1);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_1);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_1), vertices_1, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        // render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(VAO_1);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
